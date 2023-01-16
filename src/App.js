0ca53660525fd2f7f1c4d99cf834e686d8cc0512
rumen.zaechki <rumen.zaechki@pariplaydev.com>
2023-01-16 19:43:14 +0200

    Fixed some minor issues and added ChatFeed.jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { ChatEngine } from "react-chat-engine";
 import './App.css';
+import ChatFeed from "./components/ChatFeed";
 
 const App = () => {
     return (
         <ChatEngine 
             height="100vh"
-            projectID=""
-            userName=""
-            userSecret=""
+            projectID="3722fc98-d346-4be0-ab3a-d84ff323b610"
+            userName="randomdude"
+            userSecret="randompass"
+            renderChatFeed={(chatProps) => <ChatFeed {...chatProps}/>}
         />
     );
 }
+
+export default App;
