dc93c016a970eb5cf1a56a593f25a7adfca66ceb
Rumen Zaechki <mation_palaeo@aleeas.com>
2024-04-21 16:41:48 +0300

    Added methods for joining rooms, sending messages and disconnecting

--- chat_app.api/ChatApp.Api/Hubs/ChatHub.cs
+++ chat_app.api/ChatApp.Api/Hubs/ChatHub.cs
@@ -4,9 +4,50 @@ namespace ChatApp.Api;
 
 public class ChatHub : Hub
 {
+    private readonly IDictionary<string, UserConnection> _userConnections;
+
+    public ChatHub(IDictionary<string, UserConnection> userConnections)
+    {
+        _userConnections = userConnections;
+    }
     public async Task JoinRoom(UserConnection userConnection)
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
-        await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", $"{userConnection.User} has joined {userConnection.Room}.");
+
+        _userConnections[Context.ConnectionId] = userConnection;
+
+        await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", $"{userConnection.User} has joined {userConnection.Room}");
+
+        await SendConnectedUsers(userConnection.Room);
     }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        if (_userConnections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+        {
+            _userConnections.Remove(Context.ConnectionId);
+            Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", $"{userConnection.User} has left");
+
+        }
+        return base.OnDisconnectedAsync(exception);
+    }
+
+    public async Task SendMessage(string message)
+    {
+        if (_userConnections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+        {
+            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", userConnection.User, message);
+        }
+    }
+
+
+    public Task SendConnectedUsers(string room)
+    {
+        var users = _userConnections.Values
+            .Where(c => c.Room == room)
+            .Select(c => c.User);
+
+        return Clients.Group(room).SendAsync("UsersInRoom", users);
+    }
+
 }
