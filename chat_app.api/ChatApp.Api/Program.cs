9894cd54a20405aa23bc28c9c0dffcae6969e69c
Rumen Zaechki <mation_palaeo@aleeas.com>
2024-05-07 19:44:24 +0300

    Added the remaining components

--- chat_app.api/ChatApp.Api/Program.cs
+++ chat_app.api/ChatApp.Api/Program.cs
@@ -3,7 +3,24 @@ using ChatApp.Api;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
 
+builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                    {
+                        builder.WithOrigins("http://localhost:3000")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .AllowCredentials();
+                    });
+            });
+
+builder.Services.AddSingleton<IDictionary<string, UserConnection>>(opts => new Dictionary<string, UserConnection>());
+
+
 var app = builder.Build();
+
+app.UseRouting();
+app.UseCors();
 
 app.MapHub<ChatHub>("/chatHub");
 
--- chat_app.web/chat_app.web/src/App.js
+++ chat_app.web/chat_app.web/src/App.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
-import './styles/App.css';
 import Lobby from './components/Lobby';
+import Chat from './components/Chat';
+import './styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   const [connection, setConnection] = useState();
@@ -13,52 +15,53 @@ const App = () => {
       const connection = new HubConnectionBuilder()
         .withUrl("http://localhost:5269/chatHub")
         .configureLogging(LogLevel.Information)
-        .build()
+        .build();
 
       connection.on("ReceiveMessage", (user, message) => {
-        setMessages(messages => [...messages, { user, message }])
-      })
+        setMessages(messages => [...messages, { user, message }]);
+      });
 
       connection.on("UsersInRoom", (users) => {
-        setUsers(users)
-      })
+        setUsers(users);
+      });
 
       connection.onclose(e => {
-        setConnection()
-        setMessages([])
-        setUsers([])
-      })
+        setConnection();
+        setMessages([]);
+        setUsers([]);
+      });
 
-      await connection.start()
-      await connection.invoke("JoinRoom", { user, room })
-      setConnection(connection)
+      await connection.start();
+      await connection.invoke("JoinRoom", { user, room });
+      setConnection(connection);
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   }
 
   const sendMessage = async (message) => {
     try {
-      await connection.invoke("SendMessage", message)
+      await connection.invoke("SendMessage", message);
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   }
 
   const closeConnection = async () => {
     try {
-      await connection.stop()
+      await connection.stop();
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   }
 
   return <div className='app'>
     <h2>RandomChat</h2>
     <hr className='line' />
-    <Lobby joinRoom={joinRoom}></Lobby>
+    {!connection
+      ? <Lobby joinRoom={joinRoom} />
+      : <Chat sendMessage={sendMessage} messages={messages} users={users} closeConnection={closeConnection} />}
   </div>
-
 }
 
 export default App;
